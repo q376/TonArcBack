cde501d0c2740043556c2cbfcf66858ff13cc16e
q376 <141039932+q376@users.noreply.github.com>
2025-12-21 17:29:23 +0200

    Import ScoreSubmission in main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from schemas import WalletAuth
+from schemas import WalletAuth, ScoreSubmission
 from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
